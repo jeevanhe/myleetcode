d5f9ab7860f12424a11cb6e4bd4cc8b66ae635e9
Jeevan Hunsur Eswara <jeevan.h.e@gmail.com>
2022-01-22 21:59:56 -0800

    Create 15_3Sum.py

---
+++ 15_3Sum.py
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/3sum/
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums [i-1] != nums[i]:
+                self.twoSum(nums, i, result)
+        return result
+    
+    def twoSum(self, nums, i, result):
+        low = i + 1
+        high = len(nums) - 1
+        while (low < high):
+            sum = nums[i] + nums[low] + nums[high]
+            if sum < 0:
+                low += 1
+            elif sum > 0 :
+                high -= 1
+            else:
+                result.append([nums[i], nums[low], nums[high]])
+                low += 1
+                high -= 1
+                while low < high and nums[low] == nums [low-1]:
+                    low += 1
